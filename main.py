49fc395ab7e9c79a840ae5774312d8e775b17b93
fxsmith1824 <88464880+fxsmith1824@users.noreply.github.com>
2022-06-15 14:47:10 -0500

    added casefold() to fix excluded_phrases cleaning

--- main.py
+++ main.py
@@ -17,8 +17,11 @@ BEARER_TOKEN = os.getenv('BEARER_TOKEN')
 
 translate_punctuation = str.maketrans('','','!.,')
 excluded_phrases = ['GUILDWARS', 'GUILD WARS', 'GUILDWARS2', 'GUILD WARS 2', 'GW2', 'GUILD', 'WARS', '2']
+excluded_phrases = [x.casefold() for x in excluded_phrases]
 
 client = tweepy.Client(BEARER_TOKEN)
+
+count = client.get_recent_tweets_count(query='(#GW2 OR #GuildWars2) -commission -RT -#commssionart -#commission -#art lang:en -is:retweet -is:quote', granularity='day')
 
 tweets = client.search_recent_tweets(query='(#GW2 OR #GuildWars2) -commission -RT -#commssionart -#commission -#art lang:en -is:retweet -is:quote', tweet_fields=['text'], max_results=100)
 # tweets = tweepy.Paginator(client.search_recent_tweets, query='(#GW2 OR #GuildWars2) -commission -RT -#commssionart -#commission -#art lang:en -is:retweet -is:quote', tweet_fields=['text'], max_results=100).flatten(limit=1000)
@@ -35,7 +38,7 @@ for tweet in tweet_text:
     word_list = [x for x in word_list if not x.startswith('#') and not x.startswith('http') and not x.startswith('@')]
     word_list = [x for x in word_list if 'http' not in x]
     word_list = [x for x in word_list if '#' not in x]
-    word_list = [x for x in word_list if not any(phrase in x for phrase in excluded_phrases)]
+    word_list = [x for x in word_list if x.casefold() not in excluded_phrases]
     word_list = [x.translate(translate_punctuation) for x in word_list]
     words += word_list
 
